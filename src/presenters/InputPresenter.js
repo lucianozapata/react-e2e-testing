b13e6488fc8373e018d0745826fc39ce6fe18ef0
Luciano <luciano.zapata@mentimeter.com>
2022-03-28 13:47:37 +0200

    feat: added all files

---
+++ src/presenters/InputPresenter.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { ItemView } from "../views/AddItemView";
+
+export function InputPresenter() {
+  // Initial List
+  let initialList = ["Testing", "Två"];
+
+  /**
+   * Function that adds a new item to the shopping list.
+   * @param {*} newItem
+   */
+  function addToList(newItem) {
+    const newList = theList.concat(newItem);
+    setTheList(newList);
+  }
+
+  /**
+   * Remove an item from the active shopping list.
+   * @param {*} newItem
+   */
+  function removeFromList(newItem) {
+    const activeList = theList.filter((value) => {
+      return value !== newItem;
+    });
+    setTheList(activeList);
+  }
+  const [theList, setTheList] = React.useState(initialList);
+
+  return (
+    <ItemView
+      list={theList}
+      updateList={(item) => addToList(item)}
+      removeItem={(item) => removeFromList(item)}
+    />
+  );
+}
---
+++ src/views/AddItemView.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export function ItemView(props) {
+  const [name, setName] = React.useState("");
+
+  return (
+    <div id="testDiv">
+      <div></div>
+      <div id="Heading">Shopping list</div>
+      <form
+        id="flexDiv"
+        onSubmit={(e) => {
+          e.preventDefault();
+          props.updateList(name);
+          setName("");
+        }}
+      >
+        <div>
+          <label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />{" "}
+          </label>
+        </div>
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+
+      {/* The shopping list. */}
+      <div id="itemDiv">
+        {props.list.map((item, index) => {
+          return (
+            <>
+              <div id="mapList">
+                <div id="uniqueItemDiv">{item}</div>
+                <div
+                  onClick={() => {
+                    props.removeItem(item);
+                  }}
+                  id="deleteDiv"
+                >
+                  x
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
