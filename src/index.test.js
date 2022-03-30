ab3954b063ee16a0ff58506f10b7c3093c102551
Luciano <luciano.zapata@mentimeter.com>
2022-03-30 15:30:03 +0200

    feat: remoed tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,16 +1,2 @@
 import { render, screen } from "@testing-library/react";
 import { InputPresenter } from "./presenters/InputPresenter";
-
-test("Should render header", () => {
-  render(<InputPresenter />);
-  const header = screen.getByText(/Shopping list/i);
-  expect(header).toBeInTheDocument();
-});
-
-test("Length of todo equals 2", () => {
-  render(<InputPresenter />);
-  const elem1 = screen.getByText("Milk");
-  const elem2 = screen.getByText("Chocolate");
-  expect(elem1).toBeInTheDocument();
-  expect(elem2).toBeInTheDocument();
-});
