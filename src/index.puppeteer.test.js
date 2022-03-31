5541ab86d3cabe6ae82a0150338890d8aa844fe4
Luciano <luciano.zapata@mentimeter.com>
2022-03-31 20:13:52 +0200

    feat: changed localhost 2

--- src/index.puppeteer.test.js
+++ src/index.puppeteer.test.js
@@ -1,4 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { InputPresenter } from "./presenters/InputPresenter";
 const puppeteer = require("puppeteer");
-const URL = "http://localhost:3002/";
+const URL = "http://localhost:3000/";
+
+describe("First site", () => {
+  it(" Milk is in list", async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.goto(URL, { waitUntil: "domcontentloaded" });
+    const text = await page.evaluate(() => document.body.textContent);
+    expect(text).toContain("Milk");
+    expect(text).toContain("Chocolate");
+  });
+});
