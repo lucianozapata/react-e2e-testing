fb73e182c87e9117a33a32ff1a8e94e7d06e5423
Luciano <luciano.zapata@mentimeter.com>
2022-03-31 15:37:52 +0200

    feat: installed Db & puppeteer

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import { InputPresenter } from "./presenters/InputPresenter";
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  await page.screenshot({ path: "example.png" });
+
+  await browser.close();
+})();
 
 test("Should render header", () => {
   render(<InputPresenter />);
