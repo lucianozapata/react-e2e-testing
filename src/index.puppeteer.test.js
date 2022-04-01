72e6cc601541bae350291c49d97a968396e544cc
Luciano <luciano.zapata@mentimeter.com>
2022-04-01 21:00:20 +0200

    feat: testing

--- src/index.puppeteer.test.js
+++ src/index.puppeteer.test.js
@@ -1,4 +1,33 @@
-import { render, screen } from "@testing-library/react";
-import { InputPresenter } from "./presenters/InputPresenter";
 const puppeteer = require("puppeteer");
 const URL = "http://localhost:3000/";
+
+describe("First site", () => {
+  it(" Milk is in list", async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.goto(URL, { waitUntil: "domcontentloaded" });
+    const text = await page.evaluate(() => document.body.textContent);
+    expect(text).toContain("Milk");
+    expect(text).toContain("Chocolate");
+  }, 10000);
+
+  it("Add an element in shopping List", async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      slowMo: 200,
+      args: ["--no-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.goto(URL, { waitUntil: "domcontentloaded" });
+    await page.waitForSelector("input[name=inputDiv]");
+    await page.type("#katacodaid", "Eggs");
+    await page.click("#clickbutton");
+
+    const text = await page.evaluate(() => document.body.textContent);
+
+    expect(text).toContain("Eggs");
+  }, 10000);
+});
