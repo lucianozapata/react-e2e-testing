9cac6f8bb8c031999e430116b32cd59f5b3a0dc6
Luciano <luciano.zapata@mentimeter.com>
2022-04-02 09:09:32 +0200

    one test

--- src/index.puppeteer.test.js
+++ src/index.puppeteer.test.js
@@ -1,37 +1,37 @@
 const puppeteer = require("puppeteer");
 const URL = "http://localhost:3000/";
 
-describe("First site", () => {
-  // test("Milk is in list", async () => {
-  //   const browser = await puppeteer.launch({
-  //     headless: true,
-  //     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  //   });
-  //   const page = await browser.newPage();
-  //   await page.goto(URL, { waitUntil: "domcontentloaded" });
-  //   const text = await page.evaluate(() => document.body.textContent);
-  //   await browser.close();
-  //   await text.close();
-  //   await page.close();
-  //   expect(text).toContain("Milk");
-  //   expect(text).toContain("Chocolate");
-  // });
+// describe("First site", () => {
+// test("Milk is in list", async () => {
+//   const browser = await puppeteer.launch({
+//     headless: true,
+//     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+//   });
+//   const page = await browser.newPage();
+//   await page.goto(URL, { waitUntil: "domcontentloaded" });
+//   const text = await page.evaluate(() => document.body.textContent);
+//   await browser.close();
+//   await text.close();
+//   await page.close();
+//   expect(text).toContain("Milk");
+//   expect(text).toContain("Chocolate");
+// });
 
-  test("Add an element in shopping List", async () => {
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
-    const page = await browser.newPage();
+test("Add an element in shopping List", async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
 
-    page.goto(URL, { waitUntil: "domcontentloaded" });
-    await page.waitForSelector("input[name=inputDiv]");
-    await page.type("#katacodaid", "Eggs");
-    await page.click("#clickbutton");
+  page.goto(URL, { waitUntil: "domcontentloaded" });
+  await page.waitForSelector("input[name=inputDiv]");
+  await page.type("#katacodaid", "Eggs");
+  await page.click("#clickbutton");
 
-    const text = await page.evaluate(() => document.body.textContent);
+  const text = await page.evaluate(() => document.body.textContent);
 
-    expect(text).toContain("Eggs");
-    await browser.close();
-  }, 40000);
-});
+  expect(text).toContain("Eggs");
+  await browser.close();
+}, 40000);
+// });
